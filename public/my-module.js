49f15d65622ac799cc9602f508086b0c1c6e0f61
ckwwingo <99191737+wingck@users.noreply.github.com>
2023-03-24 09:56:03 -0400

    Add files via upload

--- public/my-module.js
+++ public/my-module.js
@@ -1,5 +1,3 @@
-import { theShire } from './my-script.js';
-
-import { buckland } from './my-script.js';
+import { theShire, buckland } from './my-script.js';
 
 export { theShire, statment } from './my-script.js';
