6d33287157e21f8ed486be6980b48b6fd599e178
ckwwingo <99191737+wingck@users.noreply.github.com>
2023-03-24 13:49:23 -0400

    Add files via upload

--- public/my-module.js
+++ public/my-module.js
@@ -1,4 +1,4 @@
 import { theShire, buckland } from './my-script.js';
 
 const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
-export { theShire, statement };
+export { theShire, buckland, statement };
