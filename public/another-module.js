dc1240a2c5b9ef4638897759346159cc898ae144
ckwwingo <99191737+wingck@users.noreply.github.com>
2023-03-24 10:20:55 -0400

    Add files via upload

--- public/another-module.js
+++ public/another-module.js
@@ -1,1 +1,10 @@
-export { theShire, buckland, HobbitSettlement } from './my-script.js';
+export var theShire;
+export var buckland;
+export var HobbitSettlement = class {
+	constructor(name, founding, founders) {
+		this.name = name;
+		this.founding = founding;
+		this.founders = founders;
+		this.originalSettlement = { name: 'Eriador', details: eriadorDetails };
+	}
+};
--- public/my-module.js
+++ public/my-module.js
@@ -1,3 +1,12 @@
 import { theShire, buckland } from './my-script.js';
 
-export { theShire, statment } from './my-script.js';
+export var theShire;
+
+export var HobbitSettlement = class {
+	constructor(name, founding, founders) {
+		this.name = name;
+		this.founding = founding;
+		this.founders = founders;
+		this.originalSettlement = { name: 'Eriador', details: eriadorDetails };
+	}
+};
